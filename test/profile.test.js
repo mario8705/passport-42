fafc7109846ea56f83867bee6561ba159b42c095
Adrien PACHKOFF <adrien-dev@pachkoff.com>
2016-11-08 07:31:23 +0100

    typo in test

--- test/profile.test.js
+++ test/profile.test.js
@@ -24,7 +24,7 @@ describe('Profile.parse', function() {
       expect(profile.displayName).to.equal('Count Dooku');
       expect(profile.profileUrl).to.equal('https://api.intra.42.fr/v2/users/codooku');
       expect(profile.emails).to.have.length(1);
-      expect(profile.emails[0].value).to.equal('codooku@student');
+      expect(profile.emails[0].value).to.equal('codooku@student.42.fr');
       expect(profile.photos).to.have.length(1);
       expect(profile.photos[0].value).to.equal('https://cdn.intra.42.fr/images/empty.png');
       expect(profile.phoneNumbers).to.have.length(1);
